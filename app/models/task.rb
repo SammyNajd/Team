3483b7f9c5c1bd11308c05b747277fa2715e3939
SammyNajd <sammy.najd@gmail.com>
2020-08-22 23:14:29 -0700

    Finished task core functionality. Adding extra features

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,19 @@
 class Task < ApplicationRecord
     belongs_to :user, optional: true
+    validates :user_id, :start_date, :subject, :description, :priority, :status, presence: true 
+
+    enum priority_levels: {
+        "Very Low" => 1,
+        "Low" => 2,
+        "Medium" => 3,
+        "High" => 4,
+        "Severe" => 5
+    }
+
+    enum status_levels: {
+        "Planned" => "Planned",
+        "In Progress" => "In Progress",
+        "Completed" => "Completed"
+    }
+
 end
